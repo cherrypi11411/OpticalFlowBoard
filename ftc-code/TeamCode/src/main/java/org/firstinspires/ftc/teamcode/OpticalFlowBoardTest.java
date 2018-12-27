288274a2427e88796d70302365cbe8c2e84be1a1
cherrypi11411 <cherrypi11411@gmail.com>
2018-12-27 14:10:43 -0800

    Remove unneeded imports

--- ftc-code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalFlowBoardTest.java
+++ ftc-code/TeamCode/src/main/java/org/firstinspires/ftc/teamcode/OpticalFlowBoardTest.java
@@ -2,8 +2,6 @@ package org.firstinspires.ftc.teamcode;
 
 import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
-import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorEx;
 
 import org.firstinspires.ftc.teamcode.OpticalFlowBoard;
 import org.firstinspires.ftc.teamcode.DbgLog;
